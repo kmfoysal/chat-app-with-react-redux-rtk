77518a8fe0bea599b65d0243d1f60572cc57b0f6
kmfoysal <contact.kmfoysal@gmail.com>
2022-10-05 17:51:45 +0600

    cofigure redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/apiSlice';
+import authReducer from '../features/auth/authSlice';
+import conversationsReducer from "../features/conversations/conversationsSlice";
+import messagesReducer from "../features/messages/messagesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    conversations: conversationsReducer,
+    messages: messagesReducer,
   },
+
+  // Deactivated Devtools in productions 
+
+  devTools: process.env.NODE_ENV !== "production",
+
+  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(apiSlice.middleware)
+
+  
 });
